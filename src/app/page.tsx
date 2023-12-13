d5e1d7d6dccfad4b73593258bd1a397f250057ba
Jon Low <jontatem@Jons-MacBook-Pro.local>
2023-12-13 14:59:05 -0800

    adding todo comments for implementation & minor styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {Swappable, Plugins} from '@shopify/draggable';
+import { Swappable, Plugins } from '@shopify/draggable';
 import { useEffect, useRef, useState } from 'react';
 
 const tiles = [
@@ -44,7 +44,16 @@ const tiles = [
 
 export default function Home() {
   const ref = useRef(false)
-  const [actionLog, setActionLog] = useState<any>([])
+  const [actionLog, setActionLog] = useState<any[]>([])
+  const [swappableObject, setSwappableObject] = useState<Swappable | null>(null)
+
+  const undoAction = () => {
+    // TODO: implement
+  }
+
+  const redoAction = () => {
+    // TODO: implement
+  }
 
   useEffect(() => {
     if (!ref.current) {
@@ -60,7 +69,17 @@ export default function Home() {
           },
           plugins: [Plugins.ResizeMirror],
         });
+        // TODO: implement action log within the following functions
+        swappable.on('swappable:stop', (e) => {
+          console.log(e)
+        })
+        swappable.on('swappable:swapped', (e) => {
+          console.log(e)
+        })
+        setSwappableObject(swappable)
       }
+
+      // TODO: load last saved state on page refresh
     }
     return () => {
       ref.current = true
@@ -72,14 +91,18 @@ export default function Home() {
       <h1 className='text-5xl font-bold'>Tatem Drag Test</h1>
       <div className='flex items-center justify-center gap-5'>
         <button
-          disabled={actionLog.length === 0}
+          // TODO: implement disabled logic
+          disabled={true}
           className='px-3 py-1 bg-gray-100 rounded disabled:bg-gray-300 disabled:text-gray-500 font-mono'
+          onClick={undoAction}
         >
           Undo action
         </button>
         <button
-          disabled={actionLog.length === 0}
+          // TODO: implement disabled logic
+          disabled={true}
           className='px-3 py-1 bg-gray-100 rounded disabled:bg-gray-300 disabled:text-gray-500 font-mono'
+          onClick={redoAction}
         >
           Redo action
         </button>
@@ -87,7 +110,7 @@ export default function Home() {
       <div id='draggable-container' className='grid grid-cols-4 gap-5 mx-auto w-full h-full'>
         {tiles.map((tile, idx) => {
           return (
-            <div key={idx} className={'draggable flex items-center justify-center flex-col gap-2 rounded w-full h-40 drop-shadow-lg ' + tile.color}>
+            <div key={idx} id={`box-${idx+1}`} className={'draggable cursor-pointer hover:shadow-lg flex items-center justify-center flex-col gap-2 rounded w-full h-40 drop-shadow-lg ' + tile.color}>
               <h3 className='text-2xl font-bold text-gray-100'>{idx + 1}</h3>
             </div>
           )
